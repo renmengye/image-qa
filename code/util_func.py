19614b03ef2a62cb4f573f205e528692f35649d4
renmengye <renmengye@gmail.com>
2015-01-31 21:40:55 -0500

    deleted reshape

--- code/util_func.py
+++ code/util_func.py
@@ -58,7 +58,7 @@ def crossEntIdx(Y, T):
 
 def crossEntOne(Y, T):
     eps = 1e-5
-    T = np.reshape(T, Y.shape)
+    T = T.reshape(Y.shape)
     cost = -T * np.log(Y + eps) - (1 - T) * np.log(1 - Y + eps)
     dcost = -T / (Y + eps) + (1 - T) / (1 - Y + eps)
     if len(Y.shape) == 0:
@@ -78,14 +78,14 @@ def simpleSum(Y):
 
 def simpleSumDeriv(T, Y):
     Yout = simpleSum(Y)
-    diff =  Yout - np.reshape(T, Yout.shape)
+    diff =  Yout - T.reshape(Yout.shape)
     timespan = Y.shape[-2]
     E = 0.5 * np.sum(np.power(diff, 2), axis=0) / float(timespan)
 
     if len(Y.shape) == 2:
-        dEdY = np.repeat(np.reshape(diff, (diff.shape[0], 1)), Y.shape[1], axis=1) / float(timespan)
+        dEdY = np.repeat(diff.reshape((diff.shape[0], 1)), Y.shape[1], axis=1) / float(timespan)
     elif len(Y.shape) == 3:
-        dEdY = np.repeat(np.reshape(diff, (diff.shape[0], diff.shape[1], 1)), Y.shape[2], axis=2) / float(timespan)
+        dEdY = np.repeat(diff.reshape((diff.shape[0], diff.shape[1], 1)), Y.shape[2], axis=2) / float(timespan)
     return E, dEdY
 
 def simpleSumDecision(Y):
