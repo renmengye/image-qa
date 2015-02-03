ba09e8a23285f3211292b932253e9fa33a30616d
Mengye Ren <mren@guppy5.ais.sandbox>
2015-02-03 02:29:21 -0500

    reenabled cpu

--- code/lstm_profile.py
+++ code/lstm_profile.py
@@ -4,23 +4,23 @@ import numpy as np
 import sys
 import time
 
-#start = time.time()
-#for i in range(0, 10):
-#    multiErr = len(sys.argv) > 1 and sys.argv[1] == 'm'
-#    lstm = l.LSTM(
-#        inputDim=100,
-#        outputDim=100,
-#        initRange=.1,
-#        initSeed=3,
-#        cutOffZeroEnd=True,
-#        multiErr=multiErr,
-#        outputdEdX=True)
-#    X = np.random.rand(10, 100, 100)
-#    Y = lstm.forwardPass(X)
-#    dEdY = np.random.rand(10, 100, 100) if multiErr else np.random.rand(10, 100)
-#    dEdY = lstm.backPropagate(dEdY)
+start = time.time()
+for i in range(0, 10):
+    multiErr = len(sys.argv) > 1 and sys.argv[1] == 'm'
+    lstm = l.LSTM(
+        inputDim=100,
+        outputDim=100,
+        initRange=.1,
+        initSeed=3,
+        cutOffZeroEnd=True,
+        multiErr=multiErr,
+        outputdEdX=True)
+    X = np.random.rand(10, 100, 100)
+    Y = lstm.forwardPass(X)
+    dEdY = np.random.rand(10, 100, 100) if multiErr else np.random.rand(10, 100)
+    dEdY = lstm.backPropagate(dEdY)
 #
-#print '%.4f ms' % (time.time() - start)
+print '%.4f s' % (time.time() - start)
 
 
 start = time.time()
@@ -38,4 +38,4 @@ for i in range(0, 10):
     Y = lstm.forwardPass(X)
     dEdY = np.random.rand(10, 100, 100) if multiErr else np.random.rand(10, 100)
     dEdY = lstm.backPropagate(dEdY)
-print '%.4f ms' % (time.time() - start)
+print '%.4f s' % (time.time() - start)
