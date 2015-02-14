71c905a06ca7b808917da767803bb37183250e89
renmengye <renmengye@gmail.com>
2015-02-13 20:34:38 -0500

    Added xvalid module

--- code/train.py
+++ code/train.py
@@ -1,7 +1,6 @@
 from trainer import *
 name = sys.argv[1]
 outputFolder = None
-test = False
 for i in range(2, len(sys.argv)):
     if sys.argv[i] == '-o':
         outputFolder = sys.argv[i + 1]
@@ -11,8 +10,6 @@ for i in range(2, len(sys.argv)):
         configFilename = sys.argv[i + 1]
     elif sys.argv[i] == '-g':
         useGpu = True
-    elif sys.argv[i] == '-t':
-        test = True
 
 trainData = np.load(trainDataFilename)
 trainInput = trainData[0]
