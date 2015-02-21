b55f67251464551d2cc94a4fadc1a8550b81daa1
renmengye <renmengye@gmail.com>
2015-02-21 15:35:24 -0500

    working on recurrent module

---
+++ src/nn/recurrent.py
@@ -0,0 +1,13 @@
+class Recurrent(Stage):
+    """
+    Recurrent container.
+    Propagate through time.
+    """
+    def __init__(self, stages, name=None, outputdEdX=True):
+        pass
+
+    def forward(self, X):
+        pass
+
+    def backward(self, dEdY):
+        pass
