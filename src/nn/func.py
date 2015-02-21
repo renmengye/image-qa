4158f9ff9e5117aec6acf1bb9270c9a004f6f005
renmengye <renmengye@gmail.com>
2015-02-21 11:22:01 -0500

    random word feature

--- src/nn/func.py
+++ src/nn/func.py
@@ -2,16 +2,8 @@ import numpy as np
 
 def meanSqErr(Y, T):
     diff =  Y - T.reshape(Y.shape)
-    N = 1
-    timespan = 1
-    if len(Y.shape) > 1:
-        N = Y.shape[0]
-        timespan = 1
-    if len(Y.shape) > 2:
-        N = Y.shape[0]
-        timespan = Y.shape[1]
-    E = 0.5 * np.sum(np.power(diff, 2)) / float(timespan) / float(N)
-    dEdY = diff / float(timespan) / float(N)
+    E = 0.5 * np.sum(np.power(diff, 2)) / float(Y.size)
+    dEdY = diff / float(Y.size)
     return E, dEdY
 
 def hardLimit(Y):
@@ -64,29 +56,10 @@ def crossEntOne(Y, T):
     else:
         E = np.sum(cost) / float(Y.size)
         dEdY = dcost / float(Y.size)
-
     return E, dEdY
 
 def argmax(Y):
     return np.argmax(Y, axis=-1)
-
-def simpleSum(Y):
-    return np.sum(Y, axis=-1)
-
-def simpleSumDeriv(T, Y):
-    Yout = simpleSum(Y)
-    diff =  Yout - T.reshape(Yout.shape)
-    timespan = Y.shape[-2]
-    E = 0.5 * np.sum(np.power(diff, 2), axis=0) / float(timespan)
-
-    if len(Y.shape) == 2:
-        dEdY = np.repeat(diff.reshape((diff.shape[0], 1)), Y.shape[1], axis=1) / float(timespan)
-    elif len(Y.shape) == 3:
-        dEdY = np.repeat(diff.reshape((diff.shape[0], diff.shape[1], 1)), Y.shape[2], axis=2) / float(timespan)
-    return E, dEdY
-
-def simpleSumDecision(Y):
-    return (simpleSum(Y) > 0.5).astype(int)
 
 def calcRate(model, Y, T):
     Yfinal = model.predict(Y)
