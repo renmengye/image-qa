1a31c2333ae03e91cd9715296b0158d1e01d8c8b
renmengye <renmengye@gmail.com>
2015-02-21 14:25:58 -0500

    tester

---
+++ src/nn/tester.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def test(model, X):
+    N = X.shape[0]
+    numExPerBat = 100
+    batchStart = 0
+    Y = None
+    while batchStart < N:
+        # Batch info
+        batchEnd = min(N, batchStart + numExPerBat)
+        Ytmp = model.forward(X[batchStart:batchEnd], dropout=False)
+        if Y is None:
+            Yshape = np.copy(Ytmp.shape)
+            Yshape[0] = N
+            Y = np.zeros(Yshape)
+        Y[batchStart:batchEnd] = Ytmp
+        batchStart += numExPerBat
+    return Y
+
+def calcRate(model, Y, T):
+    Yfinal = model.predict(Y)
+    correct = np.sum(Yfinal.reshape(Yfinal.size) == T.reshape(T.size))
+    total = Yfinal.size
+    rate = correct / float(total)
+    return rate, correct, total
