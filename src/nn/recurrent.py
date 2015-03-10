Fixed a bug in updating gradient. Attention gate gradient still seems to have some discrepancy

src/nn/recurrent.py
@@ -185,7 +185,9 @@ class Map_Recurrent(RecurrentSubstage):
         dEdZ = self.activeFn.backward(dEdY, self.Y, 0)
         self.dEdW = np.dot(dEdZ.transpose(), self.X)
         dEdX = np.dot(dEdZ, self.W[:, :-1])
+        #self.dEdX = dEdX
         return dEdX if self.outputdEdX else None
+
 
 class Selector_Recurrent(RecurrentSubstage):
     def __init__(self, 
@@ -415,6 +417,7 @@ class SumProduct_Recurrent(RecurrentSubstage):
         dEdY = dEdY.reshape(dEdY.shape[0], 1, dEdY.shape[1])
         dEdX.append(np.sum(dEdY * self.X[1], axis=2))
         dEdX.append(dEdY * self.X[0])
+        #self.dEdX = dEdX
         return dEdX
 
 class Recurrent(Stage):
@@ -512,10 +515,15 @@ class Recurrent(Stage):
             X = np.random.rand(2, self.timespan, self.inputDim)
         elif self.inputType == 'int':
             X = np.round(np.random.rand(2, self.timespan, self.inputDim) * 5)
+        
         self.forward(X)
         for t in range(1, self.timespan):
-            for s in range(1, len(self.stages[0])-1):
+            for s in range(1, len(self.stages[0]) - 1):
                 self.stages[t][s].W = self.stages[0][s].W
+        for t in range(self.timespan):
+            for s in range(len(self.stages[0])):
+                self.stages[t][s].X = 0
+                self.stages[t][s].Y = 0
 
     def forward(self, X):
         N = X.shape[0]
@@ -579,22 +587,24 @@ class Recurrent(Stage):
         for t in range(self.timespan):
             for stage in self.stages[t]:
                 stage.dEdY = 0.0
+                #stage.dEdX = 0.0
         for stage in self.constStages:
             stage.dEdY = 0.0
-
+            #stage.dEdX = 0.0
+        
+        # Sum error through time
         for s in range(1, len(self.stages[0]) - 1):
             if type(self.stages[0][s].W) is np.ndarray:
-                #tmp = np.zeros((self.timespan, self.stages[0][s].W.shape[0], self.stages[0][s].W.shape[1]))
+                self.dEdW[s] = np.zeros(self.stages[0][s].W.shape)
                 for t in range(self.timespan):
-                    #tmp[t] = self.stages[t][s].getGradient()
-                    self.dEdW[s] += self.stages[t][s].getGradient()
+                    self.dEdW[s] += self.stages[t][s].dEdW
                     self.stages[t][s].dEdW = 0.0
 
         # For gradient check purpose, synchronize the sum of gradient to the time=0 stage
         for s in range(1, len(self.stages[0]) - 1):
             if self.stages[0][s].learningRate > 0.0:
                 self.stages[0][s].dEdW = self.dEdW[s]
-        #self.dEdX = dEdX
+        self.dEdX = dEdX
         return dEdX if self.outputdEdX else None
 
     def updateWeights(self):

src/nn/recurrent_tests.py
@@ -177,7 +177,7 @@ class LSTM_Recurrent_Random_Tests(unittest.TestCase):
 
         random = np.random.RandomState(1)
         costFn = crossEntOne
-        for i in range(10):
+        for i in range(3):
             X = random.rand(N, Time, D)
             if multiOutput:
                 T = random.rand(N, Time, D2)
@@ -191,11 +191,15 @@ class LSTM_Recurrent_Random_Tests(unittest.TestCase):
                 Y2 = lstm2.forward(X)[:,:-1]
             else:
                 Y2 = lstm2.forward(X)
+
             E, dEdY2 = costFn(Y2, T)
             if multiOutput:
                 dEdX2 = lstm2.backward(np.concatenate((dEdY2, np.zeros((N, 1, D2))), axis=1))
             else:
                 dEdX2 = lstm2.backward(dEdY2)
+
+            # print i, 'Y', Y/Y2
+            # print i, 'dEdY', dEdY/dEdY2
 
             I2 = lstm.stageDict['I-4']
             F2 = lstm.stageDict['F-4']
@@ -207,9 +211,13 @@ class LSTM_Recurrent_Random_Tests(unittest.TestCase):
             lstm2.updateWeights()
             self.chkEqual(Y, Y2)
             self.chkEqual(dEdX, dEdX2)
+            # print i, '1', dEdW
+            # print i, '2', dEdW2
+            # print i, '3', dEdW/dEdW2
             self.chkEqual(dEdW, dEdW2)
             W = np.concatenate((I2.W, F2.W, Z2.W, O2.W), axis=-1)
             W2 = lstm2.W
+            # print i, '4', W/W2
             self.chkEqual(W, W2)
 
     def chkEqual(self, a, b):