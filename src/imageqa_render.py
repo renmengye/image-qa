83b2371a7b849423fc809d4178f721a173dc906c
Mengye Ren <mren@mail.cs.toronto.edu>
2015-04-28 23:40:19 -0400

    debug

--- src/imageqa_render.py
+++ src/imageqa_render.py
@@ -28,7 +28,7 @@ def renderHtml(
     if X.shape[0] < 1000:
         return [renderSinglePage(
             X, Y, T, questionArray, answerArray, 
-            topK, urlDict, imgidDict, 0, 1)]
+            topK, urlDict, 0, 1)]
     else:
         result = []
         numPages = X.shape[0] / 2000 + 1
@@ -38,7 +38,7 @@ def renderHtml(
             page = renderSinglePage(
                 X[start:end], Y[start:end], T[start:end], 
                 questionArray, answerArray,
-                topK, urlDict, imgidDict, i, numPages)
+                topK, urlDict, i, numPages)
             result.append(page)
         return result
 
@@ -56,7 +56,7 @@ def renderMenu(iPage, numPages):
     return ''.join(htmlList)
 
 def renderSingleItem(
-                    imageUrl, 
+                    imageFilename, 
                     questionIndex, 
                     question, 
                     correctAnswer,
@@ -156,18 +156,19 @@ def readImgDictDaquar():
 
 if __name__ == '__main__':
     """
-    Usage: imageqa_render.py {id} 
-                             -d[ata] dataFolder 
-                             -o[utput] outputFolder
+    Usage: imageqa_render.py -id {id} 
+                             -d[ata] {dataFolder}
+                             -o[utput] {outputFolder}
                              -daquar/-coco
     """
-    taskId = sys.argv[1]
     dataset = 'coco'
-    for i in range(2, len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         if sys.argv[i] == '-d' or sys.argv[i] == '-data':
             dataFolder = sys.argv[i + 1]
         elif sys.argv[i] == '-o' or sys.argv[i] == '-output':
             outputFolder = sys.argv[i + 1]
+        elif sys.argv[i] == '-id':
+            taskId = sys.argv[i + 1]
         elif sys.argv[i] == '-daquar':
             dataset = 'daquar'
         elif sys.argv[i] == '-coco':
@@ -207,7 +208,7 @@ if __name__ == '__main__':
     if not os.path.exists(htmlOutputFolder):
         os.makedirs(htmlOutputFolder)
     pages = renderHtml(inputTest, outputTest, targetTest, 
-                questionArray, answerArray, 10, urlDict, imgidDict)
+                questionArray, answerArray, 10, urlDict)
     for i, page in enumerate(pages):
         with open(os.path.join(htmlOutputFolder, 
                 htmlHyperLink % i), 'w') as f:
