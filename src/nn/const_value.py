9193740d44943371a4a0d18b0347bb617f4d16b3
Mengye Ren <rmy@localhost.localdomain>
2015-05-22 02:29:50 -0400

    change

--- src/nn/const_value.py
+++ src/nn/const_value.py
@@ -12,6 +12,7 @@ class ConstValue(Stage):
                  inputNames=inputNames,
                  outputdEdX=False)
         self.dEdW = 0
+        self.value = value
         
     def graphBackward(self):
         self.backward(self.dEdY)
