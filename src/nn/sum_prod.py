3a8b63c9f152d4de7525cb85a70281cb29c06058
Mengye Ren <mren@cluster1.ais.sandbox>
2015-09-08 20:45:17 -0400

    graph sync

--- src/nn/sum_prod.py
+++ src/nn/sum_prod.py
@@ -20,6 +20,8 @@ class SumProduct(Stage):
             outputDim=outputDim)
         self.sumAxis = sumAxis
         self.beta = beta
+        if self.sumAxis != 1 and self.sumAxis != 2:
+            raise Exception('Sum axis other than 2 or 3 is not implemented.')
 
     def getInput(self):
         # Assume that the input size is always 2
@@ -62,32 +64,51 @@ class SumProduct(Stage):
     def backward(self, dEdY):
         # Need to generalize, but now, let's assume it's the attention model.
         dEdX = []
+        if self.sumAxis == 1:
+            newshape = (dEdY.shape[0], 1, dEdY.shape[1])
+            newaxis = 2
+        elif self.sumAxis == 2:
+            newshape = (dEdY.shape[0], dEdY.shape[1], 1)
+            newaxis = 1
         if self.gpu:
             if len(self.X) == 2:
-                dEdY = dEdY.reshape(dEdY.shape[0], 1, dEdY.shape[1])
+                dEdY = dEdY.reshape(newshape)
+                #dEdY = dEdY.reshape(dEdY.shape[0], 1, dEdY.shape[1])
+                # 100, 196, 512
+                # dedy = 100, 196
+                # dedy = 100, 196, 1
+                # x0 = 100, 1, 512
+                # x1 = 100, 196, 512
                 dEdY = gpu.as_garray(dEdY)
-                dEdX1 = self.beta * gpu.sum(dEdY * self.X[1], axis=2)
+                dEdX1 = self.beta * gpu.sum(dEdY * self.X[1], axis=newaxis)
                 dEdX2 = self.beta * dEdY * self.X[0]
                 dEdX.append(dEdX1.as_numpy_array(dtype='float32'))
                 dEdX.append(dEdX2.as_numpy_array(dtype='float32'))
             elif len(self.X) == 3:
-                dEdY = gpu.as_garray(dEdY)
-                dEdY2 = dEdY.reshape(dEdY.shape[0], 1, dEdY.shape[1])
-                dEdY2 = gpu.as_garray(dEdY2)
-                dEdX1 = self.X[2] * gpu.sum(dEdY2 * self.X[1], axis=2)
-                dEdX2 = self.X[2].reshape(self.X[2].shape[0], 1, 1) * dEdY2 * self.X[0]
-                dEdX3 = gpu.sum(dEdY * self.Z, axis=-1).reshape(self.X[2].shape[0], 1)
-                dEdX.append(dEdX1.as_numpy_array(dtype='float32'))
-                dEdX.append(dEdX2.as_numpy_array(dtype='float32'))
-                dEdX.append(dEdX3.as_numpy_array(dtype='float32'))
+                raise Exception('Not supported')
+                #dEdY = gpu.as_garray(dEdY)
+                #dEdY2 = dEdY.reshape(newshape)
+                ## dEdY2 = dEdY.reshape(dEdY.shape[0], 1, dEdY.shape[1])
+                #dEdY2 = gpu.as_garray(dEdY2)
+                #dEdX1 = self.X[2] * gpu.sum(dEdY2 * self.X[1], axis=2)
+                #dEdX2 = self.X[2].reshape(
+                #    self.X[2].shape[0], 1, 1) * dEdY2 * self.X[0]
+                #dEdX3 = gpu.sum(
+                #    dEdY * self.Z, axis=-1).reshape(self.X[2].shape[0], 1)
+                #dEdX.append(dEdX1.as_numpy_array(dtype='float32'))
+                #dEdX.append(dEdX2.as_numpy_array(dtype='float32'))
+                #dEdX.append(dEdX3.as_numpy_array(dtype='float32'))
         else:
             if len(self.X) == 2:
-                dEdY = dEdY.reshape(dEdY.shape[0], 1, dEdY.shape[1])
-                dEdX.append(self.beta * np.sum(dEdY * self.X[1], axis=2))
+                dEdY = dEdY.reshape(newshape)
+                # dEdY = dEdY.reshape(dEdY.shape[0], 1, dEdY.shape[1])
+                dEdX.append(self.beta * np.sum(dEdY * self.X[1], axis=newaxis))
                 dEdX.append(self.beta * dEdY * self.X[0])
             elif len(self.X) == 3:
-                dEdY2 = dEdY.reshape(dEdY.shape[0], 1, dEdY.shape[1])
-                dEdX.append(self.X[2] * np.sum(dEdY2 * self.X[1], axis=2))
-                dEdX.append(self.X[2].reshape(self.X[2].shape[0], 1, 1) * dEdY2 * self.X[0])
-                dEdX.append(np.sum(dEdY * self.Z, axis=-1).reshape(self.X[2].shape[0], 1))
+                raise Exception('Not supported')
+                #dEdY2 = dEdY.reshape(newshape)
+                ## dEdY2 = dEdY.reshape(dEdY.shape[0], 1, dEdY.shape[1])
+                #dEdX.append(self.X[2] * np.sum(dEdY2 * self.X[1], axis=2))
+                #dEdX.append(self.X[2].reshape(self.X[2].shape[0], 1, 1) * dEdY2 * self.X[0])
+                #dEdX.append(np.sum(dEdY * self.Z, axis=-1).reshape(self.X[2].shape[0], 1))
         return dEdX
